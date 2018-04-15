f359e56666e67483811ac8dffcb41cc3295b8d5f
curder <curder@foxmail.com>
2018-04-15 13:46:48 +0800

    typo fix

--- app/Profile.php
+++ app/Profile.php
@@ -6,13 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    public function profile()
-    {
-        return $this->hasOne(Profile::class);
-    }
-
-    public function posts()
-    {
-        return $this->hasMany(Post::class);
-    }
+  
 }
